b086f92dfedb8ab8711de69a848ff470964fb3dc
Szabó Ármin András <ozelara@gmail.com>
2024-09-08 21:30:30 +0200

    Osztály létrehozása

---
+++ Lego.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegoDolog
+{
+    public class Lego
+    {
+        public string LegoID { get; set; }
+        public string Name { get; set; }
+        public string CategoryName { get; set; }
+        public string ColorName { get; set; }
+        public int Quantity { get; set; }
+
+        public Lego(string legoID, string name, string colorName, string categoryName, int quantity)
+        {
+            LegoID = legoID;
+            Name = name;
+            CategoryName = categoryName;
+            ColorName = colorName;
+            Quantity = quantity;
+        }
+    }
+}
